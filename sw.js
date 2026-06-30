e0f54d33b4683e475b1bcf54e829d7199a99479f
mgcoach <vagalsete@gmail.com>
2026-06-29 22:51:41 -0300

    Ref a/b

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NUTRI.FUEL Service Worker v3.14.0
-const CACHE_NAME = 'nutrifuel-v3-14-0-0';
+const CACHE_NAME = 'nutrifuel-v3-14-1-0';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
